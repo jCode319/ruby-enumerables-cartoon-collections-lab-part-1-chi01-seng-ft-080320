384b8888fce27f951c3a484777c71a69ad2cd008
Jarrell Jackson <jdjack3@gmail.com>
2020-07-20 21:03:02 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,7 +1,9 @@
 def greet_characters(array)
   array.each do |element|
     puts "Hello #{{element}!}"
+  end
 end
+
 
 def list_dwarves(array)
   # Use `each_with_index` to enumerate over the provided array
